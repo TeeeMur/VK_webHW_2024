6699619d8dbea056824633c64cf331c844ef7bc6
TeeMur <timurs.kzn@gmail.com>
2024-11-11 16:43:00 +0300

    added admin panel

--- vkHWApp/admin.py
+++ vkHWApp/admin.py
@@ -4,9 +4,9 @@ from .models import *
 # Register your models here.
 
 
-admin.register(Profile)
-admin.register(Tag)
-admin.register(Question)
-admin.register(Answer)
-admin.register(QuestionLike)
-admin.register(AnswerLike)
+admin.site.register(Profile)
+admin.site.register(Tag)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuestionLike)
+admin.site.register(AnswerLike)
